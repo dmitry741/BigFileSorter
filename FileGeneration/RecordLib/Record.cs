1ecd53eee82dd8cdbaac1b0059479c9ee633370e
Dmitriy Pavlov <dmitriy.pavlov@danfoss.com>
2021-12-28 09:48:37 +0300

    dev RecordLib

--- FileGeneration/RecordLib/Record.cs
+++ FileGeneration/RecordLib/Record.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace RecordLib
 {
-    public class Record
+    public class Record : IComparable<Record>
     {
+        string _strPart;
+        int _numberPart;
+        string _line;
+
+        public Record(string line)
+        {
+            _line = line;
+            string[] ar = line.Split('.');
+            _numberPart = int.Parse(ar[0]);
+            _strPart = ar[1].TrimStart();
+        }
+
+        public string Line => _line;
+
+        public int CompareTo(Record other)
+        {
+            int strCompare = _strPart.CompareTo(other._strPart);
+            return (strCompare != 0) ? strCompare : _numberPart.CompareTo(other._numberPart);
+        }
     }
 }
