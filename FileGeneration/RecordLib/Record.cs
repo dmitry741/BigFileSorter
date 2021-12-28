de52b310a12e65093f1c6f7566c0ef5f427d38fd
Dmitriy Pavlov <dmitriy.pavlov@danfoss.com>
2021-12-28 09:31:30 +0300

    Create RecordLib

--- FileGeneration/RecordLib/Record.cs
+++ FileGeneration/RecordLib/Record.cs
@@ -3,14 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using RecordLib;
 
-namespace FileGeneration
+namespace RecordLib
 {
-    class Program
+    public class Record
     {
-        static void Main(string[] args)
-        {
-        }
     }
 }
